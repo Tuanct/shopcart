3e1dc3a941cd6785cf96f9cbdda12240be1288ac
Tuanct <caothanhtuan.93@gmail.com>
2016-09-11 01:38:36 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var Product = require('../models/product');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var products = Product.find(function(err, doc){
+  		res.render('shop/index', { title: 'Shopping Cart', products: products });
+	});
+});
+
+module.exports = router;
---
+++ seed/product-seeder.js
@@ -0,0 +1,42 @@
+var Product = new require('../models/product');
+var mongoose = require('mongoose');
+var mongoose.connect('mongodb://localhost/shopcart');
+var products =[ 
+	new Product ({
+		imagePath: "poster.medium.jpg",
+		title: "Harry Postter 1",
+		description: "Harry Potter và Hòn Đá Phù Thủy là bộ phim đầu tiên trong series phim “Harry Potter” được xây dựng dựa trên tiểu thuyết của nhà văn J.K.Rowling.",
+		price: 17,
+	}),
+	new Product ({
+		imagePath: "2.jpg",
+		title: "Harry Postter 2",
+		description: "Harry Potter and the Chamber of Secrets is the second novel in the Harry Potter series, written by J. K. Rowling. ",
+		price: 18,
+	}),
+	new Product ({
+		imagePath: "3.jpg",
+		title: "Harry Postter 3",
+		description: "Harry Potter and the Prisoner of Azkaban is the third novel in the Harry Potter series, written by J. K. Rowling.",
+		price: 19,
+	}),
+	new Product ({
+		imagePath: "4.jpg",
+		title: "Harry Postter 4",
+		description: "Harry';s fourth year at Hogwarts is about to start and he is enjoying the summer vacation with his friends.",
+		price: 20,
+	}),
+];
+
+var d = 0;
+for (var i = 0; i < products.length ; i++){
+	products[i].save(function(err, result){
+		d++;
+		if ( d == products.length){
+			exit();
+		}
+	}); 
+}
+function exit(){
+	mongoose.disconnect();
+}
